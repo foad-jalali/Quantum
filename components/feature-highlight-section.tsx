7edf5e0f2e308363f7412e7633fce669ef03933a
r3ndo0 <r3ndo0@gmail.com>
2025-05-12 17:02:52 +0330

    headband ya Zahra

--- components/feature-highlight-section.tsx
+++ components/feature-highlight-section.tsx
@@ -5,79 +5,86 @@ import React from "react";
 import SimpleCard from "./simple-card";
 
 interface Feature {
-    icon: string;
-    title?: string;
-    description: string;
+  icon: string;
+  title?: string;
+  description: string;
 }
 
 interface FeatureHighlightSection {
-    title: string;
-    subtitle: string;
-    features: Feature[];
+  title: string;
+  subtitle: string;
+  features: Feature[];
 }
 
 const fadeInUp = {
-    hidden: { opacity: 0, y: 20 },
-    visible: { opacity: 1, y: 0 },
+  hidden: { opacity: 0, y: 20 },
+  visible: { opacity: 1, y: 0 },
 };
 
 const staggerContainer = {
-    visible: {
-        transition: {
-            staggerChildren: 0.2,
-        },
+  visible: {
+    transition: {
+      staggerChildren: 0.2,
     },
+  },
 };
 
-const FeatureHighlightSection = ({ title, subtitle, features }: FeatureHighlightSection) => {
-    return (
-        <section className="py-16 bg-[#F5F5F5]" id="why-us">
-            <div className="container mx-auto px-4 md:px-6">
-                <motion.h2
-                    className="text-3xl font-bold tracking-tight text-center mb-4"
-                    initial={{ opacity: 0, y: 20 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    viewport={{ once: true }}
-                    transition={{ duration: 0.5 }}
-                >
-                    <motion.span
-                        className="text-amber-500 inline-block"
-                        animate={{ rotate: [0, 10, 0, -10, 0] }}
-                        transition={{ duration: 2, repeat: Infinity, repeatDelay: 5 }}
-                    >
-                    </motion.span>{" "}
-                    {title}
-                </motion.h2>
+const palette = ["#00A676", "#D66853", "#247BA0"];
 
-                <motion.h3
-                    className="text-lg text-center text-gray-700 mb-12"
-                    initial={{ opacity: 0, y: 20 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    viewport={{ once: true }}
-                    transition={{ duration: 0.5, delay: 0.2 }}
-                >
-                    {subtitle}
-                </motion.h3>
+const colorSelector = (index: number) => palette[index % palette.length];
+const FeatureHighlightSection = ({
+  title,
+  subtitle,
+  features,
+}: FeatureHighlightSection) => {
+  return (
+    <article className="py-16 relative bg-[#F5F5F5]" id="why-us">
+      <div className="container mx-auto px-4 md:px-6">
+        <motion.h2
+          className="text-3xl font-bold tracking-tight text-center mb-4"
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.5 }}
+        >
+          <motion.span
+            className="text-amber-500 inline-block"
+            animate={{ rotate: [0, 10, 0, -10, 0] }}
+            transition={{ duration: 2, repeat: Infinity, repeatDelay: 5 }}
+          ></motion.span>
+          {title}
+        </motion.h2>
 
-                <motion.div
-                    className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4"
-                    variants={staggerContainer}
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true }}
-                >
-                    {features.map((feature, index) => (
-                        <SimpleCard
-                            key={index}
-                            icon={feature.icon}
-                            title={feature.title}
-                            description={feature.description}
-                        />
-                    ))}
-                </motion.div>
-            </div>
-        </section>
-    );
+        <motion.h3
+          className="text-lg text-center text-gray-700 mb-12"
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+        >
+          {subtitle}
+        </motion.h3>
+
+        <motion.div
+          className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4"
+          variants={staggerContainer}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+        >
+          {features.map((feature, index) => (
+            <SimpleCard
+              key={index}
+              color={colorSelector(index)}
+              icon={feature.icon}
+              title={feature.title}
+              description={feature.description}
+            />
+          ))}
+        </motion.div>
+      </div>
+    </article>
+  );
 };
 
 export default FeatureHighlightSection;
--- components/simple-card.tsx
+++ components/simple-card.tsx
@@ -7,6 +7,7 @@ interface SimpleCardProps {
   icon?: string;
   title?: string;
   description: string;
+  color: string;
 }
 
 const fadeInUp = {
@@ -14,13 +15,17 @@ const fadeInUp = {
   visible: { opacity: 1, y: 0 },
 };
 
-const SimpleCard = ({ icon, title, description }: SimpleCardProps) => {
+const SimpleCard = ({ icon, title, description, color }: SimpleCardProps) => {
   return (
     <motion.div
-      className="bg-white p-6 rounded-lg border border-white hover:border-amber-500 transition-colors group shadow-lg shadow-black/5"
+      className="bg-white p-6 overflow-hidden rounded-[12px] border relative border-white hover:border-amber-500 transition-colors group shadow-lg shadow-black/5"
       variants={fadeInUp}
       whileHover={{ y: -10, transition: { duration: 0.2 } }}
     >
+      <div
+        style={{ backgroundColor: color }}
+        className="h-[150%] rotate-[-9deg] absolute -left-7 top-0 bg-black w-12"
+      ></div>
       <motion.div
         className="h-16 w-16 bg-white rounded-full flex items-center justify-center mx-auto mb-4 group-hover:bg-amber-500/20 transition-colors"
         whileHover={{ scale: 1.1, rotate: 5 }}
@@ -28,9 +33,7 @@ const SimpleCard = ({ icon, title, description }: SimpleCardProps) => {
       >
         <img src={icon} alt="Feature Icon" className="w-12 h-12" />
       </motion.div>
-      <h3 className="font-semibold mb-2 text-center text-gray-900">
-        {title}
-      </h3>
+      <h3 className="font-semibold mb-2 text-center text-gray-900">{title}</h3>
       <p className="text-gray-700 text-sm text-center">{description}</p>
     </motion.div>
   );
