4d3192d1cb1f0699bd88a04960376b787e29768d
r3ndo0 <r3ndo0@gmail.com>
2025-05-17 21:23:41 +0330

    logos added , putting them in was left undone

--- app/solutions/page.tsx
+++ app/solutions/page.tsx
@@ -9,87 +9,93 @@ import PartnersSection from "@/components/partners";
 import { LogoSlider } from "@/components/ui/logo-slider";
 
 export async function getSolutions() {
-    const filePath = path.join(process.cwd(), "db", "solutions.json");
-    const data = await fs.readFile(filePath, "utf8");
-    return JSON.parse(data);
+  const filePath = path.join(process.cwd(), "db", "solutions.json");
+  const data = await fs.readFile(filePath, "utf8");
+  return JSON.parse(data);
 }
 export const metadata: Metadata = generateMetadata({
-    title: "Solutions | Quantum",
-    description: "Explore Quantum's tailored solutions for government procurement, AI-powered platforms, IT systems, and multi-industry sourcing. Discover how we deliver precision, speed, and innovation across sectors.",
-    url: "https://quantumints.com/solutions",
-    image: "/solutions/hero.png",
-    keywords: "Quantum Solutions, Government Procurement, AI Platforms, IT Services, Industrial Sourcing, Vendor Network, Public Sector Projects, Technology Integration, Procurement Experts, Quantum International Services"
+  title: "Solutions | Quantum",
+  description:
+    "Explore Quantum's tailored solutions for government procurement, AI-powered platforms, IT systems, and multi-industry sourcing. Discover how we deliver precision, speed, and innovation across sectors.",
+  url: "https://quantumints.com/solutions",
+  image: "/solutions/hero.png",
+  keywords:
+    "Quantum Solutions, Government Procurement, AI Platforms, IT Services, Industrial Sourcing, Vendor Network, Public Sector Projects, Technology Integration, Procurement Experts, Quantum International Services",
 });
 
 interface Logo {
-    id: string;
-    image: string;
+  alt: string;
+  image: string;
 }
 
 interface Solution {
-    id: string;
-    title: string;
-    description: string;
-    first_paragraph: string;
-    second_paragraph: string;
-    image: string;
-    logos: Logo[];
+  id: string;
+  title: string;
+  description: string;
+  first_paragraph: string;
+  second_paragraph: string;
+  image: string;
+  logos: Logo[];
 }
 
-
 export default async function FaqPage() {
-    const solutions = await getSolutions();
-    return (
-        <>
-            <HeroSection
-                image={solutions.hero_image}
-                title={solutions.title}
-                subtitle={solutions.subtitle}
-                buttonLink="/contact"
-            />
-            {/* <section className="py-16" style={{ backgroundColor: "#FFFFFF" }}>
+  const solutions = await getSolutions();
+  return (
+    <>
+      <HeroSection
+        image={solutions.hero_image}
+        title={solutions.title}
+        subtitle={solutions.subtitle}
+        buttonLink="/contact"
+      />
+      {/* <section className="py-16" style={{ backgroundColor: "#FFFFFF" }}>
                 <div className="container mx-auto px-4 text-center">
                     <h1 className="text-4xl font-bold mb-4 text-[#00204E]">{solutions.title}</h1>
                     <h3 className="container text-xl text-[#00204E]">{solutions.subtitle}</h3>
                 </div>
             </section> */}
 
+      {(solutions.solutions as Solution[]).map((solution, i) => {
+        const bgColor = i % 2 === 0 ? "#C9D1D3" : "#FFFFFF";
+        const sectionId =
+          solution.id || solution.title.toLowerCase().replace(/\W+/g, "-");
 
-            {(solutions.solutions as Solution[]).map((solution, i) => {
-                const bgColor = i % 2 === 0 ? "#C9D1D3" : "#FFFFFF";
-                const sectionId = solution.id || solution.title.toLowerCase().replace(/\W+/g, "-");
-
-                return (
-                    <section
-                        key={i}
-                        id={sectionId}
-                        style={{ backgroundColor: bgColor }}
-                        className="py-16"
-                    >
-                        <FeatureSection
-                            image={solution.image || "/placeholder.jpg"}
-                            alt={solution.title}
-                            description={
-                                <>
-                                    <h2 className="text-2xl font-bold mb-4 text-[#00204E]">{solution.title}</h2>
-                                    <p>{solution.description}</p>
-                                    <p>{solution.first_paragraph}</p>
-                                    <p>{solution.second_paragraph}</p>
-                                </>
-                            }
-                            reverse={i % 2 !== 1}
-                            backgroundColor="transparent"
-                            textColor="#00204E"
-                            buttonText="Get a Quote"
-                            buttonLink="/contact"
-                        />
-                        {solution?.logos && (
-                            <LogoSlider logos={solution.logos} direction="left" speed="fast" />
-                        )}
-                    </section>
-                );
-            })}
-
-        </>
-    );
+        return (
+          <section
+            key={i}
+            id={sectionId}
+            style={{ backgroundColor: bgColor }}
+            className="py-16"
+          >
+            <FeatureSection
+              image={solution.image || "/placeholder.jpg"}
+              alt={solution.title}
+              description={
+                <>
+                  <h2 className="text-2xl font-bold mb-4 text-[#00204E]">
+                    {solution.title}
+                  </h2>
+                  <p>{solution.description}</p>
+                  <p>{solution.first_paragraph}</p>
+                  <p>{solution.second_paragraph}</p>
+                </>
+              }
+              reverse={i % 2 !== 1}
+              backgroundColor="transparent"
+              textColor="#00204E"
+              buttonText="Get a Quote"
+              buttonLink="/contact"
+            />
+            {solution?.logos && (
+              <LogoSlider
+                logos={solution.logos}
+                direction="left"
+                speed="fast"
+              />
+            )}
+          </section>
+        );
+      })}
+    </>
+  );
 }
